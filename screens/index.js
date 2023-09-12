d29af48c5030bc3247fe05835318490f665397c5
Crowdbotics <engineering@crowdbotics.com>
2023-09-12 05:19:19 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,180 +1,85 @@
 import React, { useState } from "react";
 import {
   Text,
-  View,
   StyleSheet,
+  View,
+  Image,
   TextInput,
-  ScrollView,
-  Pressable,
-  Image
+  Pressable
 } from "react-native";
 
-const SubscriptionTrial = () => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+const RateTheAppScreen = (params) => {
+  const [review, setReview] = useState("");
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Text style={styles.heading}>Free tiral period</Text>
-        <View style={styles.daysContainer}>
-          <Text style={styles.days}>30</Text>
-          <Text>Days</Text>
-        </View>
-        <Text style={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non at sed.
+      <View style={styles.header}>
+        <Text style={styles.heading}>How was your experience?</Text>
+        <Text style={styles.subhHeading}>
+          Use 5 star rating to rate an app or leave a text review.
         </Text>
-        <View style={styles.separator}>
-          <View style={styles.bar} />
-          <Text style={styles.separatorText}>Or</Text>
-          <View style={styles.bar} />
-        </View>
-        <Text style={styles.periodHeading}>Choose your period</Text>
-        <View style={styles.inputsContainer}>
-          <Input
-            text="Start date"
-            value={startDate}
-            onChange={setStartDate}
-            containerStyle={styles.input}
-          />
-          <Input
-            text="End date"
-            value={endDate}
-            onChange={setEndDate}
-            containerStyle={styles.input}
-          />
-        </View>
-        <Button buttonText="Ok" style={styles.button} />
-      </ScrollView>
+      </View>
+      <Image
+        source={require("./assets/5starsImage.png")}
+        style={styles.image}
+      />
+      <Image
+        source={require("./assets/separator.png")}
+        style={styles.separator}
+      />
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Text review</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setReview(text)}
+          value={review}
+          placeholder="Enter"
+          placeholderTextColor="#9B9B9B"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+      </View>
+      <Button buttonText={"Submit"} />
+      <Button buttonText={"Cancel"} outline={true} />
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
-  },
-  heading: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center"
-  },
-  daysContainer: {
-    width: 130,
-    height: 130,
-    borderRadius: 70,
-    borderColor: "#12D790",
-    borderWidth: 12,
-    justifyContent: "center",
-    alignItems: "center",
-    alignSelf: "center",
-    marginVertical: 20
-  },
-  days: {
-    fontSize: 36,
-    fontWeight: "bold"
-  },
-  description: {
-    fontSize: 14,
-    color: "#888888",
-    textAlign: "center",
-    paddingHorizontal: 40
-  },
-  separator: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 20
-  },
-  bar: {
-    width: 100,
-    height: 1,
-    backgroundColor: "#e6e6e6"
-  },
-  separatorText: {
-    fontSize: 14,
-    color: "#888888",
-    marginHorizontal: 30
-  },
-  periodHeading: {
-    fontSize: 18,
-    color: "#000",
-    textAlign: "center",
-    marginVertical: 10,
-    fontWeight: "bold"
-  },
-  inputsContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    padding: 20
-  },
-  input: {
-    flex: 1,
-    marginHorizontal: 5
-  },
-  button: {
-    marginHorizontal: 40,
-    marginTop: 60,
-    marginBottom: 20
-  }
-});
-
-export default SubscriptionTrial;
-
-const Input = props => {
-  return (
-    <View style={[inputStyles.inputContainer, props.containerStyle]}>
-      {props.text
-        ? (
-        <Text style={inputStyles.inputText}>{props.text}</Text>
-          )
-        : null}
-
-      <TextInput
-        style={[
-          inputStyles.input,
-          props.style,
-          props.textArea ? inputStyles.textArea : null
-        ]}
-        placeholder={props.placeholder ? props.placeholder : "Enter"}
-        value={props.value}
-        onChangeText={() => props.onChange()}
-        placeholderTextColor={
-          props.placeholderTextColor ? props.placeholderTextColor : "#9B9B9B"
-        }
-        editable={props.editable !== false}
-        autoCapitalize="none"
-        autoCorrect={false}
-        multiline={!!props.textArea}
-        backgroundColor={props.backgroundColor}
-        secureTextEntry={props.secureTextEntry}
-      />
-      {props.errorText
-        ? (
-        <Text style={inputStyles.error}>{props.errorText}</Text>
-          )
-        : null}
-      {props.icon
-        ? (
-        <Pressable
-          onPress={() => props.iconOnPress()}
-          style={inputStyles.iconWithText}>
-          <Image source={props.icon} style={inputStyles.icon} />
-        </Pressable>
-          )
-        : null}
-      <View style={styles.children}>{props.children}</View>
-    </View>
-  );
-};
-
-const inputStyles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "column",
+    backgroundColor: "#fff",
     justifyContent: "center"
   },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 50
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  subhHeading: {
+    fontSize: 15,
+    lineHeight: 20,
+    marginTop: 10,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  image: {
+    alignSelf: "center"
+  },
+  separator: {
+    alignSelf: "center",
+    marginVertical: 30
+  },
+  inputContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    marginHorizontal: 20
+  },
   inputText: {
-    fontSize: 14,
+    fontSize: 16,
     marginLeft: 20,
     color: "#111112"
   },
@@ -186,90 +91,51 @@ const inputStyles = StyleSheet.create({
     paddingLeft: 20,
     marginVertical: 10,
     width: "100%",
-    height: 50,
-    color: "#000"
-  },
-  iconWithText: {
-    position: "absolute",
-    right: 30,
-    bottom: 25,
-    width: 20,
-    height: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 1
-  },
-  icon: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain"
-  },
-  textArea: {
     height: 150
-  },
-  children: {}
+  }
 });
+export default RateTheAppScreen;
 
-const Button = params => {
-  const backgroundColor = params.backgroundColor || "#000";
-  const textColor = params.textColor || "#fff";
+const Button = ({ onPress, buttonText, outline }) => {
   const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.borderColor || backgroundColor,
+    backgroundColor: outline ? "#fff" : "#000",
+    borderColor: outline ? "#000" : "#fff",
     borderWidth: 1
   };
   const btnText = {
-    color: textColor
+    color: outline ? "#000" : "#fff"
   };
   return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
-          </Text>
-          <View style={styles.childrenContainer}>{params.children}</View>
-        </Pressable>
-      </View>
+    <View style={buttonStyles.btnContainer}>
+      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={onPress}>
+        <Text style={[buttonStyles.btnText, btnText]}>{buttonText}</Text>
+      </Pressable>
     </View>
   );
 };
 
 const buttonStyles = StyleSheet.create({
   btnContainer: {
-    justifyContent: "center"
-  },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
-    borderRadius: 10
+    paddingTop: 10,
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    marginTop: 20
   },
   btn: {
+    backgroundColor: "black",
     height: 50,
+    width: "100%",
     padding: 10,
     paddingHorizontal: 25,
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
-
-    flexDirection: "row"
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    elevation: 10
   },
   btnText: {
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
   }
 });
